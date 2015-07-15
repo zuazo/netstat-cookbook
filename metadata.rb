29042961195e5131a682d7ac2e5d9e7440aced9f
Xabier de Zuazo <xabier@zuazo.org>
2015-07-15 16:46:04 +0200

    version bumped to 0.2.0 WiP

--- metadata.rb
+++ metadata.rb
@@ -24,7 +24,7 @@ maintainer_email 'xabier@zuazo.org'
 license 'Apache 2.0'
 description 'Installs netstat network tool. Recommended for Docker containers.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0' # WiP
 
 supports 'amazon'
 supports 'centos'
